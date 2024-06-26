a8a2e7ef12e31aa8161cda028c255b2d42a41ad6
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-06-26 23:38:13 +0700

    Create Barang.java

---
+++ Barang.java
@@ -0,0 +1,37 @@
+package Barang;
+
+class Barang {
+    private String kode, nama, harga, stok;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getStok() {
+        return stok;
+    }
+
+    public void setStok(String stok) {
+        this.stok = stok;
+    }
+}
